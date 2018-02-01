7a87b7aae772fdb850d9eef247ba95c76cc86f71
Samuel Warnock <samuel.warnock@illuminancesolutions.com.au>
2018-02-01 10:04:04 +0800

    Add project files.

---
+++ DependencySearch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XrmToolBox.Extensibility;
+using XrmToolBox.Extensibility.Interfaces;
+
+namespace DependencySearch
+{
+	public class DependencySearch : PluginBase {
+		public string GetAssemblyQualifiedName() {
+			throw new NotImplementedException();
+		}
+
+		public string GetCompany() {
+			throw new NotImplementedException();
+		}
+
+		public override IXrmToolBoxPluginControl GetControl() {
+			throw new NotImplementedException();
+		}
+		
+		public string GetVersion() {
+			throw new NotImplementedException();
+		}
+	}
+}
